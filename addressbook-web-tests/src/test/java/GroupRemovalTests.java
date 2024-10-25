e590b7051599c82da8e99ce532bdaf1dba39b6ca
Dmitrii Krutov <dm.krutov@mail.ru>
2024-10-25 20:02:51 +0300

    Задание №6: Реализовать двухуровневую архитектуру тестового набора v1
    Создан класс TestBase

--- addressbook-web-tests/src/test/java/GroupRemovalTests.java
+++ addressbook-web-tests/src/test/java/GroupRemovalTests.java
@@ -1,33 +1,8 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GroupRemovalTests {
-    private WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("http://localhost/addressbook/");
-        driver.manage().window().setSize(new Dimension(1920, 1013));
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.findElement(By.linkText("Logout")).click();
-        driver.quit();
-
-    }
-
+public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
@@ -50,12 +25,5 @@ public class GroupRemovalTests {
         driver.findElement(By.linkText("group page")).click();
     }
 
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
-    }
+
 }
