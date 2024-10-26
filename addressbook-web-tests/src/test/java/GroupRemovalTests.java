79b3dae8f019b378dcd4bf23aaaed2c59cc94ce2
Dmitrii Krutov <dm.krutov@mail.ru>
2024-10-26 12:50:37 +0300

    Задание №6: Реализовать двухуровневую архитектуру тестового набора v3
    Создание класса GroupData

--- addressbook-web-tests/src/test/java/GroupRemovalTests.java
+++ addressbook-web-tests/src/test/java/GroupRemovalTests.java
@@ -1,18 +1,15 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
         openGroupPage();
-        if (!isElementPresent(By.name("selected[]"))) {
-            createGroup("group name", "group header", "group footer");
+        if (!isGroupPresent()) {
+            createGroup(new GroupData("group name", "group header", "group footer"));
         }
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.name("delete")).click();
-        driver.findElement(By.linkText("group page")).click();
+        removeGroup();
     }
 
 
--- addressbook-web-tests/src/test/java/TestBase.java
+++ addressbook-web-tests/src/test/java/TestBase.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -7,6 +8,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
     protected static WebDriver driver;
+
+    protected static void removeGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
 
     @BeforeEach
     public void setUp() {
@@ -30,14 +37,14 @@ public class TestBase {
         }
     }
 
-    protected void createGroup(String group_name, String group_header, String group_footer) {
+    protected void createGroup(GroupData group) {
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys(group_name);
+        driver.findElement(By.name("group_name")).sendKeys(group.name());
         driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys(group_header);
+        driver.findElement(By.name("group_header")).sendKeys(group.header());
         driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys(group_footer);
+        driver.findElement(By.name("group_footer")).sendKeys(group.footer());
         driver.findElement(By.name("submit")).click();
         driver.findElement(By.linkText("group page")).click();
     }
@@ -46,5 +53,9 @@ public class TestBase {
         if (!isElementPresent(By.name("new"))) {
             driver.findElement(By.linkText("groups")).click();
         }
+    }
+
+    protected boolean isGroupPresent() {
+        return isElementPresent(By.name("selected[]"));
     }
 }
