7a54bb7879738ad7fd05953abfaf4c334c9a6c76
Dmitrii Krutov <dm.krutov@mail.ru>
2024-11-05 22:13:15 +0300

    Задание №10: Реализовать проверки путём сравнения списков v2

--- addressbook-web-tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactCreationTests.java
@@ -16,7 +16,7 @@ public class ContactCreationTests extends TestBase {
 
     public static List<ContactData> contactProvider() {
         var result = new ArrayList<ContactData>();
-        /*for (var firstName : List.of("", "firstName")) {
+        for (var firstName : List.of("", "firstName")) {
             for (var lastName : List.of("", "lastName")) {
                 for (var address : List.of("", "address")) {
                     for (var email : List.of("", "email@email")) {
@@ -32,15 +32,26 @@ public class ContactCreationTests extends TestBase {
                     }
                 }
             }
-        }*/
+        }
         for (int i = 0; i < 3; i++) {
-            result.add(new ContactData()
-                    .withFirstName(randomString(i * 10))
-                    .withLastName(randomString(i * 10))
-                    .withMiddleName(randomString(i * 10))
-                    .withAddress(randomString(i * 10))
-                    .withEmail(randomString(i * 10))
-                    .withPhone(randomInt(11)));
+            if (i == 0) {
+                result.add(new ContactData()
+                        .withFirstName(randomString(i))
+                        .withLastName(randomString(i))
+                        .withMiddleName(randomString(i))
+                        .withAddress(randomString(i))
+                        .withEmail(randomString(i))
+                        .withPhone(randomInt(i)));
+            } else {
+                result.add(new ContactData()
+                        .withFirstName(randomString(i * 10))
+                        .withLastName(randomString(i * 10))
+                        .withMiddleName(randomString(i * 10))
+                        .withAddress(randomString(i * 10))
+                        .withEmail(randomString(i * 10))
+                        .withPhone(randomInt(11)));
+            }
+
         }
         return result;
     }
